42a75460d672ae50785782635b8e4247bbb5a74a
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-11-08 23:15:30 +0500

    feat(script.js): create book card with attributes

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ const addBook = function addBookToTheLibrary(src, name, author, pages, status) {
     const newBook = new Book(src, name, author, `${pages} pages`, status);
     myLibrary.push(newBook);
 }
+
+const displayBooks = function displayBooksToTheLibrary() {
+    myLibrary.forEach((book) => {
+        const bookImg = document.createElement('img');
+        const bookCard = document.createElement('div');
+        const bookTitle = document.createElement('h2');
+        const bookAuthor = document.createElement('h3');
+        const bookPages = document.createElement('p');
+        const bookStatus = document.createElement('p');
+        const bookEditBtn = document.createElement('button');
+        const bookId = crypto.randomUUID();
+
+        bookImg.src = book.src;
+        bookTitle.textContent = book.name;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        bookStatus.textContent = book.status;
+        bookEditBtn.textContent = 'Edit';
+
+        bookCard.classList.add('book__card')
+        bookCard.id = bookId;
+        bookCard.append(bookImg, bookTitle, bookAuthor, bookPages, bookStatus, bookEditBtn);
+        mainDisplay.appendChild(bookCard);
+    });
+
+}
