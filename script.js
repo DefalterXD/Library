79687d8cc7c4d06d6970ba8b8a5d350fbd7c21bf
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-11-09 10:29:40 +0500

    refactor(script.js): move card creation method into prototype

--- script.js
+++ script.js
@@ -13,33 +13,63 @@ const Book = function(src, name, author, pages, status) {
     this.status = status;
 }
 
+Book.prototype = {
+    createCard() {
+        const bookInfoContainer = document.createElement('div');
+        const readerInfoContainer = document.createElement('div');
+
+        const bookImg = document.createElement('img');
+        const bookCard = document.createElement('div');
+        const bookTitle = document.createElement('h2');
+        const bookAuthor = document.createElement('h4');
+
+        const bookPagesContainer = document.createElement('div');
+        const bookPagesTitle = document.createElement('p');
+        const bookPages = document.createElement('p');
+
+        const bookStatusContainer = document.createElement('div');
+        const bookStatusTitle = document.createElement('p');
+        const bookStatus = document.createElement('p');
+
+        const bookEditBtn = document.createElement('button');
+
+        bookImg.src = this.src;
+        bookTitle.textContent = this.name;
+        bookAuthor.textContent = this.author;
+
+        bookPagesTitle.textContent = 'Pages:';
+        bookPages.textContent = this.pages;
+
+        bookStatusTitle.textContent = 'Status:';
+        bookStatus.textContent = this.status;
+
+        bookEditBtn.textContent = 'Edit';
+        
+        bookCard.id = this.id;
+        bookCard.classList.add('book__card');
+        bookInfoContainer.classList.add('book__info');
+        readerInfoContainer.classList.add('reader__info');
+        bookEditBtn.classList.add('edit__btn');
+
+        bookInfoContainer.append(bookTitle, bookAuthor);
+        bookPagesContainer.append(bookPagesTitle, bookPages);
+        bookStatusContainer.append(bookStatusTitle, bookStatus);
+        readerInfoContainer.append(bookPagesContainer, bookStatusContainer);
+
+        bookCard.append(bookImg, bookInfoContainer, readerInfoContainer, bookEditBtn);
+
+        mainDisplay.appendChild(bookCard);
+    }
+}
+
 const addBook = function addBookToTheLibrary(src, name, author, pages, status) {
-    const newBook = new Book(src, name, author, `${pages} pages`, status);
+    const newBook = new Book(src, name, `by ${author}`, `${pages} pages`, status);
     myLibrary.push(newBook);
 }
 
 const displayBooks = function displayBooksToTheLibrary() {
     myLibrary.forEach((book) => {
-        const bookImg = document.createElement('img');
-        const bookCard = document.createElement('div');
-        const bookTitle = document.createElement('h2');
-        const bookAuthor = document.createElement('h3');
-        const bookPages = document.createElement('p');
-        const bookStatus = document.createElement('p');
-        const bookEditBtn = document.createElement('button');
-        const bookId = crypto.randomUUID();
-
-        bookImg.src = book.src;
-        bookTitle.textContent = book.name;
-        bookAuthor.textContent = book.author;
-        bookPages.textContent = book.pages;
-        bookStatus.textContent = book.status;
-        bookEditBtn.textContent = 'Edit';
-
-        bookCard.classList.add('book__card')
-        bookCard.id = bookId;
-        bookCard.append(bookImg, bookTitle, bookAuthor, bookPages, bookStatus, bookEditBtn);
-        mainDisplay.appendChild(bookCard);
+        book.createCard();
     });
 
 }
